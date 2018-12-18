0739111c43a83f5861510bb3fc1f09eb1c7a8245
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-12-19 02:23:30 +0530

    Download the German traffic data set & extract features

---
+++ constants.py
@@ -0,0 +1,4 @@
+TRAINING_SET_URL = 'http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Training_Images.zip'
+TESTING_SET_URL = 'http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_Images.zip'
+TRAINING_SET_FILE = 'GTSRB_Final_Training_Images.zip'
+TESTING_SET_FILE = 'GTSRB_Final_Test_Images.zip'
